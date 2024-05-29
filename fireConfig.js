82c19754b0bfd371c77566b6cca16c596955ad58
Anurag Lanjewar <85513169+anulanjewar0504@users.noreply.github.com>
2024-05-29 11:30:17 +0530

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAF4Nx-_3noh67i773npHaCRVR1f1buwaY",
+  authDomain: "crud-e3f9d.firebaseapp.com",
+  databaseURL: "https://crud-e3f9d-default-rtdb.firebaseio.com",
+  projectId: "crud-e3f9d",
+  storageBucket: "crud-e3f9d.appspot.com",
+  messagingSenderId: "142277040672",
+  appId: "1:142277040672:web:4275256a4eea9495fcba45",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
